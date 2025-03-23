bcd3c5d344646b99510661d66b22edd71032ac88
JuanArgotyTobar <argotydaniel@gmail.com>
2025-03-22 19:03:36 -0500

    creacion de archivo en la carpeta pages y modificacion del archivo main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.title("Presupuesto Personal")
-st.write("Bienvenido a tu aplicación de presupuesto")
+
+st.write("Utiliza el menú de la izquierda para navegar a las diferentes secciones de la aplicación.")
+if st.button('Ir al Formulario'):
+    st.experimental_set_query_params(page='formulario')
---
+++ app/pages/pg_vista_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Vista 1")
+
+st.write("Aquí se mostrarán los reportes de los presupuestos ingresados.")
+# Aquí puedes agregar la lógica para recuperar y mostrar datos de la base de datos
