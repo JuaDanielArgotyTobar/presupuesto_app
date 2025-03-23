c0ac75b35abad0517eaf2b288e1416f72f5c79f9
JuanArgotyTobar <argotydaniel@gmail.com>
2025-03-23 10:29:22 -0500

    se organiza la pagina en la carpeta /app

--- app/main.py
+++ app/main.py
@@ -5,14 +5,6 @@ st.set_page_config(page_title="Presupuesto personal",
 
 
 st.title("Creando ingreso de datos")
-st.markdown(
-    """
-    Puedo usar latex
-    $$
-    \sum_{k_0}^{n-1} a^2
-    $$
-    """
-)
 
 nuevo_gasto = st.Page("tools/ingresar_gasto.py",
                       title="Registrar gasto",
