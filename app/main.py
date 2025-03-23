abd1939848b1120597f2f567a1944572e5062a92
JuanArgotyTobar <argotydaniel@gmail.com>
2025-03-23 10:01:19 -0500

    Prueba conecxion a la base de datos supabase

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Presupuesto personal", page_icon="💰")
+st.set_page_config(page_title="Presupuesto personal",
+                   page_icon="💰")
